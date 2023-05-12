240bdd3ffbe965ed5dd8806b4e00b3b0f28fcad1
ValerioPelosin <valeriopelo07@gmail.com>
2023-05-12 13:06:26 +0200

    Added p5.js code

--- sketch.js
+++ sketch.js
@@ -13,20 +13,59 @@ function preload() {
 
 function setup() {
   createCanvas3D(windowWidth, windowHeight);
-  
-  //Change background color of the scene
-  background3D("#333333");
-  
-  hideGrid();
-  
-  var tree = clone(treeModel,0,0,0);
+  environment (SUNSET);
 
+  var albero = beginGroup();
+  diffuse("#0D4B0F");
+  roughness (0,1);
+  sphere(0, 5, 0, 3);
+  roughness (1);
+  diffuse("#44291F");
+  align(TOP);
+  cylinder(0, 0, 0, 1, 4);
+  endGroup();
+
+  var pino = beginGroup();
+  diffuse("#0D4B0F");
+  roughness (0,1);
+  cone(5, 4, 0, 2, 5);
+  roughness (1);
+  diffuse("#44291f");
+  cylinder(5, 0, 0, 1, 4);
+  align(TOP);
+  endGroup();
+
+  var lato = 80;
+  var numeroAlberi = 40;
+  for (let i = 0; i < numeroAlberi; i++) {
+    let x = random(-lato / 2, lato / 2);
+    let y = 0;
+    let z = random(-lato / 2, lato / 2);
+    let nuovoAlbero = clone(albero, x, y, z);
+    nuovoAlbero.setScale(1, 2);
+  }
+
+  var latop = 60;
+  var numeroPini = 40;
+  for (let i = 0; i < numeroPini; i++)
+  {
+    let x = random(-latop / 2, latop / 2);
+    let y = 0;
+    let z = random(-latop / 2, latop / 2);
+    let nuovoPino = clone(pino, x, y, z);
+    nuovoPino.setScale(1, 3);
+  }
+  var terreno = beginGroup();
   align(BOTTOM);
-  diffuse("grey");
-  box(0, 0, 0, 6, 0.5, 6);
+  diffuse("#0D5310");
+  box (0,0,0,lato, 1, lato);
+  endGroup();
+  pushFX(BLOOM, 0.2);
 }
 
-function draw() {
+  
+function draw() {}
+
   // 🔍 Never create objects here, but don't forget to have it in your code:
   // leave empty for the moment
-}
+
